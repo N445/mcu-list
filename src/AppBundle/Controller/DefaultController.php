d82866d8fb4ac3dac02d0a2095a7813cec7b3bac
Robin Parisot <parisrob@hotmail.fr>
2018-06-24 19:57:32 +0200

    add margin by type

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -11,14 +11,6 @@ class DefaultController extends Controller
     public function homepageAction()
     {
         $videos  = $this->getDoctrine()->getManager()->getRepository('AppBundle:Video')->getVideoByOrdre();
-        $em      = $this->getDoctrine()->getManager();
-        $slugify = new Slugify();
-        /** @var Video $video */
-        foreach ($videos as $video) {
-            $video->setTypeClass($slugify->slugify($video->getType()));
-            $em->persist($video);
-        }
-        $em->flush();
         return $this->render('@App/default/homepage.html.twig', [
             'videos' => $videos,
         ]);
